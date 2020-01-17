5cbc85b72722c8b9148f1ae2ae5b69f21fe620fe
Code313-Detroit <59835937+Code313-Detroit@users.noreply.github.com>
2020-01-16 23:28:41 -0500

    Initial Commit

--- test.ts
+++ test.ts
@@ -1,1 +1,8 @@
 // tests go here; this will not be compiled when this package is used as a library
+ledStrip.SetLed(1, ledStrip.Color.Blue)
+ledStrip.SetLed(2, ledStrip.Color.Red)
+ledStrip.SetLed(3, ledStrip.Color.Green)
+ledStrip.SetLed(4, ledStrip.Color.White)
+ledStrip.SetLed(5, ledStrip.Color.Purple)
+ledStrip.SetLed(6, ledStrip.Color.Cyan)
+ledStrip.SetLed(7, ledStrip.Color.Yellow)
